9ea3f239fe8c28bb2f0f455c6bb2c72633c2b740
Ajaxparmar <ajaxparmar@gmail.com>
2026-04-29 18:49:44 +0530

    new next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  // This can help with some proxy/HMR issues in dev environments
   async headers() {
     return [
       {
